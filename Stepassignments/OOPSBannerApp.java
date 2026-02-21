7b109fbdf5147ece15f41c82919c7f1805af05aa
krishkudchadkar <kk7814@srmist.edu.in>
2026-02-21 14:51:54 +0530

    commit

---
+++ Stepassignments/OOPSBannerApp.java
@@ -0,0 +1,15 @@
+package Stepassignments;
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("  * * " + "     * *  " + "  * * * " + "    * * * ");
+        System.out.println(" *   * " + "   *   *  " + " *    * " + "  *     *");
+        System.out.println("*     * " + " *     * " + " *    * " + "  *      ");
+        System.out.println("*      * " + "*     * " + " * * * " + "    * * * ");
+        System.out.println("*     * " + " *     * " + " *      " + "        *");
+        System.out.println(" *   * " + "   *   *  " + " *      " + "  *     *");
+        System.out.println("  * * " + "     * *  " + "  *      " + "   **** ");
+
+    }
+}
